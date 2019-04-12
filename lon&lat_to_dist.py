76a9671aa7ee852a1da3b8fa161c5d88aa2f0bf2
kunkun_1230 <chengkun1230@163.com>
2019-04-12 14:07:55 +0800

    Add files via upload

---
+++ lon&lat_to_dist.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 09:42:55 2019
+
+@author: 18131808
+"""
+
+
+from math import *
+
+def get_distance(origin, destination):
+    # 根据经纬度计算两个点距离
+    lon1 = radians(float(destination[0]))
+    lon2 = radians(float(origin[0]))
+    lat1 = radians(float(destination[1]))
+    lat2 = radians(float(origin[1]))
+    dlon = lon1 - lon2
+    dlat = lat1 - lat2
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    dist = 2 * asin(sqrt(a))*6371*1000
+    return dist
